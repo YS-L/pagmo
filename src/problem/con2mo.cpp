eb21649dfc1d648d4eaeafd9ce7f66308a041f28
Jeremie Labroquere <jeremie.labroquere@inria.fr>
2013-06-19 03:18:01 +0200

    Implementation of con2mo in PaGMO.

---
+++ src/problem/con2mo.cpp
@@ -0,0 +1,194 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2013 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#include <cmath>
+#include <algorithm>
+
+#include "../exceptions.h"
+#include "../types.h"
+#include "base.h"
+#include "con2mo.h"
+
+namespace pagmo { namespace problem {
+
+/**
+ * Constructor using initial constrained problem
+ *
+ * @param[in] problem base::problem to be modified to use a constrained to
+ * multi-objective handling technique.
+ * @param[in] method method_type to be modified to use a simple constrained
+ * to multi-objective if defined with SIMPLE and a Coello constrained to
+ * multi-objective with COELLO.
+ *
+ */
+con2mo::con2mo(const base &problem, const method_type method):
+	base((int)problem.get_dimension(),
+		 problem.get_i_dimension(),
+		 problem.get_f_dimension() + problem.get_c_dimension(),
+		 0,
+		 0,
+		 0.),
+	m_original_problem(problem.clone()),
+	m_method(method)
+{
+	if(m_original_problem->get_c_dimension() <= 0){
+		pagmo_throw(value_error,"The original problem has no constraints.");
+	}
+
+	if( method > 1 || method < 0) {
+		pagmo_throw(value_error, "the constrained to multi-objective method must be set to 0 for simple constrained to multi-objective or to 1 for Coello constrained to multi-objective problem.");
+	}
+
+	set_bounds(m_original_problem->get_lb(),m_original_problem->get_ub());
+
+}
+
+/// Copy Constructor. Performs a deep copy
+con2mo::con2mo(const con2mo &prob):
+	base((int)prob.get_dimension(),
+		 prob.get_i_dimension(),
+		 prob.get_f_dimension(),
+		 prob.get_c_dimension(),
+		 prob.get_ic_dimension(),
+		 prob.get_c_tol()),
+	m_original_problem(prob.m_original_problem->clone()),
+	m_method(prob.m_method)
+{
+	set_bounds(m_original_problem->get_lb(),m_original_problem->get_ub());
+}
+
+/// Clone method.
+base_ptr con2mo::clone() const
+{
+	return base_ptr(new con2mo(*this));
+}
+
+/// Implementation of the objective functions.
+/// (Wraps over the original implementation)
+void con2mo::objfun_impl(fitness_vector &f, const decision_vector &x) const
+{
+	constraint_vector c(m_original_problem->get_c_dimension(),0.);
+	m_original_problem->compute_constraints(c,x);
+
+	decision_vector original_f(m_original_problem->get_f_dimension(),0.);
+	m_original_problem->objfun(original_f,x);
+
+	fitness_vector::size_type original_nbr_obj = original_f.size();
+	constraint_vector::size_type number_of_constraints = c.size();
+
+	// in all cases, the first objectives holds the initial objectives
+	for(f_size_type i=0; i<original_nbr_obj; i++) {
+		f[i] = original_f.at(i);
+	}
+
+	switch(m_method)
+	{
+	case SIMPLE:
+	{
+		for(c_size_type i=0; i<number_of_constraints; i++) {
+			f[original_nbr_obj+i] = std::max(0., c.at(i));
+		}
+		break;
+	}
+	case COELLO:
+	{
+		constraint_vector::size_type number_of_violated_constraints = 0;
+
+		// computes the number of satisfied constraints
+		for(c_size_type i=0; i<number_of_constraints; i++){
+			if(!m_original_problem->test_constraint(c,i))
+				number_of_violated_constraints += 1;
+		}
+
+		for(c_size_type i=0; i<number_of_constraints; i++) {
+			if(c.at(i) > 0.) {
+				f[original_nbr_obj+i] = c.at(i);
+			} else if(number_of_violated_constraints != 0) {
+				f[original_nbr_obj+i] = number_of_violated_constraints;
+			} else {
+				f[original_nbr_obj+i] = 0.;
+				for(f_size_type j=0; j<original_nbr_obj; j++) {
+					f[original_nbr_obj+i] += original_f.at(j);
+				}
+			}
+		}
+		break;
+	}
+	default:
+		pagmo_throw(value_error, "Error: There are only 2 methods for the constraints to multi-objective!");
+		break;
+	}
+}
+
+/// Implementation of the fc implementation.
+/// (Wraps over the original implementation)
+bool con2mo::compare_fc_impl(const fitness_vector &f1, const constraint_vector &c1, const fitness_vector &f2, const constraint_vector &c2) const
+{
+	return m_original_problem->compare_fc(f1,c1,f2,c2);
+}
+
+/// Extra human readable info for the problem.
+/**
+ * Will return a formatted string containing the type of constraint handling
+ */
+std::string con2mo::human_readable_extra() const
+{
+	std::ostringstream oss;
+	oss << m_original_problem->human_readable_extra() << std::endl;
+	oss << "\n\tConstraints handled with constraints to multi-objective, method ";
+	switch(m_method){
+	case SIMPLE: {
+		oss << "SIMPLE ";
+		break;
+	}
+	case COELLO: {
+		oss << "COELLO ";
+		break;
+	}
+	}
+	oss << std::endl;
+	return oss.str();
+}
+
+std::string con2mo::get_name() const
+{
+	std::string method;
+
+	switch(m_method){
+	case SIMPLE: {
+		method = "SIMPLE ";
+		break;
+	}
+	case COELLO: {
+		method = "COELLO ";
+		break;
+	}
+	}
+	return m_original_problem->get_name() + " [con2mo, method_" + method + "]";
+}
+
+}}
+
+BOOST_CLASS_EXPORT_IMPLEMENT(pagmo::problem::con2mo);
+
---
+++ src/problem/con2mo.h
@@ -0,0 +1,101 @@
+/*****************************************************************************
+ *   Copyright (C) 2004-2013 The PaGMO development team,                     *
+ *   Advanced Concepts Team (ACT), European Space Agency (ESA)               *
+ *   http://apps.sourceforge.net/mediawiki/pagmo                             *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Developers  *
+ *   http://apps.sourceforge.net/mediawiki/pagmo/index.php?title=Credits     *
+ *   act@esa.int                                                             *
+ *                                                                           *
+ *   This program is free software; you can redistribute it and/or modify    *
+ *   it under the terms of the GNU General Public License as published by    *
+ *   the Free Software Foundation; either version 2 of the License, or       *
+ *   (at your option) any later version.                                     *
+ *                                                                           *
+ *   This program is distributed in the hope that it will be useful,         *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *   GNU General Public License for more details.                            *
+ *                                                                           *
+ *   You should have received a copy of the GNU General Public License       *
+ *   along with this program; if not, write to the                           *
+ *   Free Software Foundation, Inc.,                                         *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.               *
+ *****************************************************************************/
+
+#ifndef PAGMO_PROBLEM_CON2MO_H
+#define PAGMO_PROBLEM_CON2MO_H
+
+#include <string>
+
+#include "../serialization.h"
+#include "../types.h"
+#include "cec2006.h"
+#include "base.h"
+
+namespace pagmo{ namespace problem {
+
+/// Constrainted to Multi-Objective meta-problem
+/**
+ * Implements a meta-problem class that wraps some other constrained problems,
+ * resulting in multi-objective problem.
+ *
+ * Two implementations of the constrained to multi-objective are available. For a problem with m constraints,
+ * m+1 objective functions are defined with the objective function as first objective and clipped positive
+ * constraints values for others. The second one is the constrained to nulti-objective defined by
+ * Coello Coello, where the objectives due to constraints includes number of violated constraints and
+ * objective function as well.
+ *
+ * @see Coello Coello, C. A. (2002). Theoretical and numerical constraint-handling techniques used with evolutionary algorithms: a survey of the state of the art. Computer methods in applied mechanics and engineering, 191(11), 1245-1287.
+ *
+ * @see Coello, C. A. C. (2000). Treating constraints as objectives for single-objective evolutionary optimization. Engineering Optimization+ A35, 32(3), 275-308.
+ *
+ * @author Jeremie Labroquere (jeremie.labroquere@gmail.com)
+ */
+
+class __PAGMO_VISIBLE con2mo : public base
+{
+	public:
+		/// Type of constraints to multi-objective.
+		/**
+		* Definition of two types of constrained to multi-objective.
+		* Simple distributes the objective and constraints to m+1 objective functions
+		* defined with the objective function as first objective and clipped positive
+		* constraints values for others. COELLO one is the same as Simple but
+		* objectives due to constraints includes number of violated constraints and
+		* objective function as well
+		*/
+		//death penalty type simple or kuri
+		enum method_type {SIMPLE = 0, COELLO = 1};
+
+		//constructors
+		con2mo(const base & = cec2006(4), const method_type = SIMPLE);
+
+		//copy constructor
+		con2mo(const con2mo &);
+		base_ptr clone() const;
+		std::string get_name() const;
+
+	protected:
+		std::string human_readable_extra() const;
+		void objfun_impl(fitness_vector &, const decision_vector &) const;
+		bool compare_fc_impl(const fitness_vector &, const constraint_vector &, const fitness_vector &, const constraint_vector &) const;
+
+	private:
+		friend class boost::serialization::access;
+		template <class Archive>
+		void serialize(Archive &ar, const unsigned int)
+		{
+			ar & boost::serialization::base_object<base>(*this);
+			ar & m_original_problem;
+			ar & const_cast<method_type &>(m_method);
+		}
+		base_ptr m_original_problem;
+
+		const method_type m_method;
+};
+
+}} //namespaces
+
+BOOST_CLASS_EXPORT_KEY(pagmo::problem::con2mo);
+
+#endif // PAGMO_PROBLEM_CON2MO_H
